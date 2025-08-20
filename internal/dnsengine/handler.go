abfb4ea7e71b7bf03fc3773b3a31bdbf4b602e10
Roshan Singh <rosh.s568@gmail.com>
2025-08-20 12:14:57 +0530

    Refactor logging in DNS handler and server to use structured logger

--- internal/dnsengine/handler.go
+++ internal/dnsengine/handler.go
@@ -1,46 +1,45 @@
 package dnsengine
 
 import (
-	"fmt"
-
 	"github.com/lopster568/phantomDNS/internal/config"
+	"github.com/lopster568/phantomDNS/internal/logger"
 
 	"github.com/miekg/dns"
 )
 
 func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
 	if r == nil || len(r.Question) == 0 {
-		fmt.Println("Received empty DNS request")
+		logger.Log.Warn("Received empty DNS request")
 		return
 	}
 
 	domain := r.Question[0].Name
-	fmt.Println("Received request for domain:", domain)
+	logger.Log.Infof("Received request for domain: %s", domain)
 
 	cfg, err := config.Load("configs/config.yaml")
 	if err != nil {
-		fmt.Println("Failed to load config:", err)
+		logger.Log.Errorf("Failed to load config: %v", err)
 		return
 	}
 
 	client := new(dns.Client)
 	resp, _, err := client.Exchange(r, cfg.DataPlane.UpstreamResolvers[0])
 	if err != nil {
-		fmt.Println("Failed to query upstream resolver:", err)
+		logger.Log.Errorf("Failed to query upstream resolver: %v", err)
 
 		// Avoid silent dropping of query which makes it hard to debug for the client
 		m := new(dns.Msg)
 		m.SetRcode(r, dns.RcodeServerFailure)
 		if err := w.WriteMsg(m); err != nil {
-			fmt.Println("Failed to write response for dropped query:", err)
+			logger.Log.Errorf("Failed to write response for dropped query: %v", err)
 		}
 		return
 	}
 
 	if err := w.WriteMsg(resp); err != nil {
-		fmt.Println("Failed to write response:", err)
+		logger.Log.Errorf("Failed to write response: %v", err)
 		return
 	}
 
-	fmt.Println("Upstream resolvers:", cfg.DataPlane.UpstreamResolvers)
+	logger.Log.Infof("Upstream resolvers: %v", cfg.DataPlane.UpstreamResolvers)
 }
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,13 +2,16 @@ package logger
 
 import (
 	"os"
+
 	"github.com/sirupsen/logrus"
 )
 
-func New() *logrus.Logger {
+func configureLogger() *logrus.Logger {
 	log := logrus.New()
 	log.SetOutput(os.Stdout)
 	log.SetFormatter(&logrus.JSONFormatter{})
 	log.SetLevel(logrus.InfoLevel)
 	return log
 }
+
+var Log = configureLogger()
