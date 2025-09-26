6d09a18003842e0bf18d0614eb1786da46a97615
Roshan <rosh.s568@gmail.com>
2025-09-26 19:23:30 +0000

    Basic engine execution successful

--- cmd/dataplane/main.go
+++ cmd/dataplane/main.go
@@ -16,12 +16,7 @@ func main() {
 	db.InitDB("/app/data/phantomdns.db")
 	// 2. Initialize Repositories (store)
 	repos := repositories.NewStore(db.DB)
-	// 3. Initialize DNS Engine with default config and repos
-	engine, err := dnsengine.NewDNSEngine(config.DefaultConfig.DataPlane, repos)
-	if err != nil {
-		logger.Log.Fatal("Failed to create DNS engine: " + err.Error())
-	}
-	// 4. Initialize Policy Engine
+	// 3. Initialize Policy Engine
 	policyEngine := policy.NewPolicyEngine()
 	policies, err := policy.LoadPoliciesFromFile("/app/configs/policies.json")
 	if err != nil {
@@ -29,6 +24,11 @@ func main() {
 	}
 	if err := policyEngine.LoadPolicies(policies); err != nil {
 		logger.Log.Fatalf("failed to load snapshot: %v", err)
+	}
+	// 4. Initialize DNS Engine with default config and repos
+	engine, err := dnsengine.NewDNSEngine(config.DefaultConfig.DataPlane, repos, policyEngine)
+	if err != nil {
+		logger.Log.Fatal("Failed to create DNS engine: " + err.Error())
 	}
 	// 5. Initialize and Run Server with the engine
 	srv, err := dnsengine.NewServer(config.DefaultConfig.DataPlane, engine)
