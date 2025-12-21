b6241c8df2c542b7304ff3ddcff8a9e788d75f33
Roshan Singh <rosh.s568@gmail.com>
2025-12-21 16:48:05 +0000

    Fix makefile | add router

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	go build -o ./bin/ ./cmd/scanner/main.go
+	go build -o bin/scanner/scanner ./cmd/scanner
 
 run:
-	./bin/scanner
+	./bin/scanner/scanner
--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/PhantomDNS/scanner/internal/scanner"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
+
+	r.GET("/scan", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Scan endpoint"})
+	})
+
+	r.Run(":8080")
+
 	s := &scanner.Scanner{}
 	_, _ = s.Scan(context.Background())
 }
