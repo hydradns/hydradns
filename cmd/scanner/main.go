3ab1b09007fe8876ea810257070750fee0013a78
Roshan Singh <rosh.s568@gmail.com>
2025-12-21 17:24:59 +0000

    context time implemented

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/PhantomDNS/scanner/internal/scanner"
 	"github.com/gin-gonic/gin"
@@ -12,6 +13,19 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/scan", func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(
+			c.Request.Context(),
+			5*time.Second,
+		)
+		defer cancel()
+
+		select {
+		case <-ctx.Done():
+			c.JSON(http.StatusRequestTimeout, gin.H{"error": "Request timed out"})
+			return
+		default:
+		}
+
 		c.JSON(http.StatusOK, gin.H{"message": "Scan endpoint"})
 	})
 
