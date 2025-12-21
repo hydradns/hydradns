5f695366e1c0c8e56a220a9b5f775d0ba4d9e960
Roshan Singh <rosh.s568@gmail.com>
2025-12-21 17:35:03 +0000

    Add system resolver parser

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"net"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/PhantomDNS/scanner/internal/scanner"
@@ -29,8 +33,42 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"message": "Scan endpoint"})
 	})
 
+	resolver, err := SystemResolver("/etc/resolv.conf")
+	if err != nil {
+		panic(err)
+	}
+
+	println("System resolver:", resolver.String())
+
+	if err != nil {
+		panic(err)
+	}
+
 	r.Run(":8080")
 
 	s := &scanner.Scanner{}
 	_, _ = s.Scan(context.Background())
+}
+
+func SystemResolver(path string) (net.IP, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		fields := strings.Fields(line)
+		if len(fields) >= 2 && fields[0] == "nameserver" {
+			ip := net.ParseIP(fields[1])
+			if ip != nil {
+				return ip, nil
+			}
+		}
+	}
+
+	return nil, nil
 }
